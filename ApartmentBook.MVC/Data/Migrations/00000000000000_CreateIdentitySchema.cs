e53b68678bdb4f9e26c7d657489d13a7a3f9a847
Maciej Winnik <maciekwin3@gmail.com>
2022-05-26 17:35:04 +0200

    Change project to MVC

--- ApartmentBook.MVC/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ ApartmentBook.MVC/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace ApartmentBook.Data.Migrations
+namespace ApartmentBook.MVC.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
